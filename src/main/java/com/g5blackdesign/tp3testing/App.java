86085f607ba3cd221e228e02c7fc96ca89ef7e0a
Federico Gallardo <fedegallar2006@hotmail.com>
2020-06-06 17:11:33 -0300

    first commit

---
+++ src/main/java/com/g5blackdesign/tp3testing/App.java
@@ -0,0 +1,17 @@
+package com.g5blackdesign.tp3testing;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/g5blackdesign/tp3testing/AppTest.java
@@ -0,0 +1,23 @@
+package com.g5blackdesign.tp3testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void TestPrueba(){
+        System.out.println("\n----ESTO ES UNA PRUEBA----\n");
+    }
+}
