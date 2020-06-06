d94485decc967f67ae6d831ac9fc52279481cba8
Federico Gallardo <fedegallar2006@hotmail.com>
2020-06-06 18:13:18 -0300

    Archivo donde hay pruebas

--- src/test/java/com/g5blackdesign/tp3testing/AppTest.java
+++ src/test/java/com/g5blackdesign/tp3testing/AppTest.java
@@ -1,8 +1,12 @@
 package com.g5blackdesign.tp3testing;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 /**
  * Unit test for simple App.
@@ -15,9 +19,9 @@ class AppTest {
     void testApp() {
         assertEquals(1, 1);
     }
-
     @Test
     void TestPrueba(){
         System.out.println("\n----ESTO ES UNA PRUEBA----\n");
     }
+
 }
