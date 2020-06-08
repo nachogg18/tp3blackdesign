4f86df20cb8633395f89a92203753b4887f60246
fedegallar <fedegallar2006@hotmail.com>
2020-06-08 17:37:09 -0300

    Agregado entidad carrito

---
+++ src/main/java/com/g5blackdesign/tp3testing/Entidades/Carrito.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g5blackdesign.tp3testing.Entidades;
+
+import java.util.List;
+
+/**
+ *
+ * @author federico
+ */
+public class Carrito {
+    private int id;
+    private List<DetalleCarrito> listdetallecarrito;
+}
