2678dc83294519ab62cb5489124f103bbf1da235
fedegallar <fedegallar2006@hotmail.com>
2020-06-08 17:59:18 -0300

    Getters y Setters

--- src/main/java/com/g5blackdesign/tp3testing/Entidades/DetalleCarrito.java
+++ src/main/java/com/g5blackdesign/tp3testing/Entidades/DetalleCarrito.java
@@ -13,4 +13,35 @@ public class DetalleCarrito {
     private int idproducto;
     private int idcarrito;
     private int cantidad;
+
+    public DetalleCarrito(int idproducto, int idcarrito, int cantidad) {
+        this.idproducto = idproducto;
+        this.idcarrito = idcarrito;
+        this.cantidad = cantidad;
+    }
+
+    public int getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public int getIdcarrito() {
+        return idcarrito;
+    }
+
+    public void setIdcarrito(int idcarrito) {
+        this.idcarrito = idcarrito;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
 }
