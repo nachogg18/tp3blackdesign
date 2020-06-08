386ac01f9974892df897476c402c7df05cdc5a0c
fedegallar <fedegallar2006@hotmail.com>
2020-06-08 17:37:33 -0300

    Añadido detalle carrito

---
+++ src/main/java/com/g5blackdesign/tp3testing/Entidades/DetalleCarrito.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g5blackdesign.tp3testing.Entidades;
+
+/**
+ *
+ * @author federico
+ */
+public class DetalleCarrito {
+    private int idproducto;
+    private int idcarrito;
+    private int cantidad;
+}
