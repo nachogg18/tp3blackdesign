54cbf90fc8b3cc532288d637484861fec379d663
fedegallar <fedegallar2006@hotmail.com>
2020-06-08 18:22:41 -0300

    Pruebas para carrito

---
+++ src/test/java/com/g5blackdesign/tp3testing/CarritoTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.g5blackdesign.tp3testing;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+/**
+ *
+ * @author federico
+ */
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class CarritoTest {
+}
