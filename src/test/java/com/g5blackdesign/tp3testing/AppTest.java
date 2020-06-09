7957ade1a8e08c48e1d916711926d40380a4cba8
Maria Luz <marialuztejada055@gmail.com>
2020-06-09 10:52:30 -0300

    tests agregados

--- src/test/java/com/g5blackdesign/tp3testing/AppTest.java
+++ src/test/java/com/g5blackdesign/tp3testing/AppTest.java
@@ -1,14 +1,17 @@
 package com.g5blackdesign.tp3testing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import com.g5blackdesign.tp3testing.Controladores.ControladorProducto;
+import com.g5blackdesign.tp3testing.Entidades.Producto;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 
 /**
@@ -17,4 +20,18 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class AppTest {
 
+    public Producto p1=new Producto("comida", "galletas", 0);
+   
+    @Test
+    public void testNombreNoDebeEstarVacio(){
+        assertNotEquals("", p1.getNombre());
+    }
+
+    @Test
+    public void testPrecioProductoMayorIgualCero() {
+
+        float precioReal = p1.getPrecio();
+        assertEquals(true, precioReal>=0);
+        
+   }
 }
