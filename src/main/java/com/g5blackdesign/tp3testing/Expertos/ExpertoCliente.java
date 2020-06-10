8af86783364276d439c69dc7015223dc45146dca
nachogg18 <ignacio.galvez@alumnos.frm.utn.edu.ar>
2020-06-10 20:56:14 -0300

    agregada prueba unitaria 'nombreCliente sólo letras' y agregado método 'soloContieneLetras' en expertoCliente'

--- src/main/java/com/g5blackdesign/tp3testing/Expertos/ExpertoCliente.java
+++ src/main/java/com/g5blackdesign/tp3testing/Expertos/ExpertoCliente.java
@@ -17,8 +17,22 @@ public class ExpertoCliente {
     public Carrito carrito;
     
     
+    public static boolean contieneSoloLetras(String nombre) {
+        for (int x = 0; x < nombre.length(); x++) {
+            char c = nombre.charAt(x);
+            // Si no está entre a y z, ni entre A y Z, ni es un espacio
+            if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ')) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    
+    
     public boolean CrearCliente(Carrito carrito, String nombre, String apellido, String dni, String email){
-        if (!carrito.getListdetallecarrito().isEmpty()) {
+        if (!carrito.getListdetallecarrito().isEmpty() && ExpertoCliente.contieneSoloLetras(nombre)) {
             usuario = new Cliente(carrito,nombre,apellido,dni,email);
             return true;
         }
--- src/test/java/com/g5blackdesign/tp3testing/ClienteTest.java
+++ src/test/java/com/g5blackdesign/tp3testing/ClienteTest.java
@@ -6,10 +6,11 @@
 package com.g5blackdesign.tp3testing;
 import com.g5blackdesign.tp3testing.Entidades.Carrito;
 import com.g5blackdesign.tp3testing.Entidades.Cliente;
+import com.g5blackdesign.tp3testing.Entidades.Producto;
 import com.g5blackdesign.tp3testing.Entidades.DetalleCarrito;
 import com.g5blackdesign.tp3testing.Controladores.ControladorCliente;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-// import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -27,6 +28,7 @@ public class ClienteTest {
         static Cliente usuario;
         static List<DetalleCarrito> listaDetalleCarrito;
         static ControladorCliente controladorCliente; 
+        static Producto producto1;
         
   
         
@@ -36,13 +38,42 @@ public class ClienteTest {
          * no se crea el cliente
          * y el método Crear cliente debe devolver false 
         ***/
+        
+        @BeforeEach
+            void crearObjetosIniciales() {
+                
+                controladorCliente = new ControladorCliente();
+                producto1 = new Producto("PC","i5 con 8 gb",10000f);
+                producto1.setId(1);
+                listaDetalleCarrito = new ArrayList();
+                
+                
+        }
+        
+        
+        
+        
+        
+        
         @ParameterizedTest
         @CsvSource({"luis,Alvarado,35678392,lafg@mail.com"})
             void CrearClienteConCarroVacío(final String nombreCliente, final String apellidoCliente, final String dniCliente,final String mailCliente) {
-                listaDetalleCarrito = new ArrayList();  //ESTÁ VACÍO
                 carrito = new Carrito(listaDetalleCarrito);
+                carrito.setId(1);
                 controladorCliente = new ControladorCliente();
                 assertEquals(false,controladorCliente.getExpertoCliente().CrearCliente(carrito, nombreCliente, apellidoCliente, dniCliente, mailCliente));
         }
+            
+        @ParameterizedTest
+        @CsvSource({"S3RG10,Alvarado,35678392,lafg@mail.com"})
+            void CrearClienteConNombreconNumeros(final String nombreCliente, final String apellidoCliente, final String dniCliente,final String mailCliente) {
+                carrito = new Carrito(listaDetalleCarrito);
+                carrito.setId(1);
+                DetalleCarrito detalleCarrito = new DetalleCarrito(1,1,3);
+                listaDetalleCarrito.add(detalleCarrito);                
+                assertEquals(false,controladorCliente.getExpertoCliente().CrearCliente(carrito, nombreCliente, apellidoCliente, dniCliente, mailCliente));
+        }
+            
+        
        
 }
